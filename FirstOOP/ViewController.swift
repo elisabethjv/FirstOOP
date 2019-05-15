d4e1ee48c823936f434006d02507273deaee7d32
Elisabeth Jessica Valencia <elisabethtsy29@gmail.com>
2019-05-15 09:24:06 +0700

    Initial Commit

---
+++ FirstOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstOOP
+//
+//  Created by Elisabeth Jessica Valencia on 15/05/19.
+//  Copyright © 2019 Elisabeth Jessica Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
